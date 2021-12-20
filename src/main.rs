249ab7e84f9a86c58c393a6b9e2db859aaaa2ea5
Thanh <quang.thanh.ta@pr1meti.de>
2021-12-20 23:04:10 +0100

    feat(lsp): Add example LSP

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,37 @@
-use tree_sitter::{Parser, Point, Query, QueryCursor};
+use tower_lsp::jsonrpc::Result;
+use tower_lsp::lsp_types::*;
+use tower_lsp::{Client, LanguageServer, LspService, Server};
 
-fn main() {
-    println!("Hello, world!");
-    let mut parser = Parser::new();
+#[derive(Debug)]
+struct Backend {
+    client: Client,
+}
 
-    parser.set_language(tree_sitter_ccsc::language()).unwrap();
-
-    let input = include_str!("input/ccsc-multiple-functions.c");
-    let tree = parser.parse(input, None).unwrap();
-
-    // List all functions in input
-    let mut cursor = QueryCursor::new();
-    let query = Query::new(
-        tree_sitter_ccsc::language(),
-        "(function_definition 
-            type: (_) @function_return_type
-            declarator: (function_declarator 
-                declarator: (identifier) @function_name
-                parameters: (parameter_list) @function_parameters
-                )
-            body: (compound_statement) @function_body) @function",
-    )
-    .unwrap();
-
-    println!("Capture names used in query:");
-    for capture in query.capture_names().iter() {
-        println!("{}", capture);
+#[tower_lsp::async_trait]
+impl LanguageServer for Backend {
+    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+        Ok(InitializeResult::default())
     }
 
-    let return_type_idx = query
-        .capture_index_for_name("function_return_type")
-        .unwrap();
-    let name_idx = query.capture_index_for_name("function_name").unwrap();
-    let parameters_idx = query.capture_index_for_name("function_parameters").unwrap();
-    let body_idx = query.capture_index_for_name("function_body").unwrap();
-    let function_idx = query.capture_index_for_name("function").unwrap();
-
-    for el in cursor.matches(&query, tree.root_node(), input.as_bytes()) {
-        let return_type_node = el.nodes_for_capture_index(return_type_idx).next().unwrap();
-        let name_node = el.nodes_for_capture_index(name_idx).next().unwrap();
-        let parameters_node = el.nodes_for_capture_index(parameters_idx).next().unwrap();
-        let body_node = el.nodes_for_capture_index(body_idx).next().unwrap();
-        let function_node = el.nodes_for_capture_index(function_idx).next().unwrap();
-
-        let return_type = return_type_node.utf8_text(input.as_bytes()).unwrap();
-        let name = name_node.utf8_text(input.as_bytes()).unwrap();
-        let parameters = parameters_node.utf8_text(input.as_bytes()).unwrap();
-        let body = body_node.utf8_text(input.as_bytes()).unwrap();
-        let function = function_node.utf8_text(input.as_bytes()).unwrap();
-
-        println!("{} {}{}", return_type, name, parameters);
-        println!("name: {}", name);
-        println!("function return type: {}", return_type);
-        println!("function parameters: {}", parameters);
-        println!("function body: ```\n{}\n```", body);
-        println!("\n```\n{}\n```", function);
+    async fn initialized(&self, _: InitializedParams) {
+        self.client
+            .log_message(MessageType::Info, "server initialized!")
+            .await;
     }
+
+    async fn shutdown(&self) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let stdin = tokio::io::stdin();
+    let stdout = tokio::io::stdout();
+
+    let (service, messages) = LspService::new(|client| Backend { client });
+    Server::new(stdin, stdout)
+        .interleave(messages)
+        .serve(service)
+        .await;
 }
