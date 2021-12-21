85b03f118b82fe9398f4011d5e5a94ba73a24bc8
Thanh <quang.thanh.ta@pr1meti.de>
2021-12-21 13:29:34 +0100

    refactor: Extract module to file
    - Remove log4rs in favor of built-in VS Code logging

---
+++ src/lsp_ccs_c.rs
@@ -0,0 +1,55 @@
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use tower_lsp::lsp_types::Url;
+use tower_lsp::Client;
+use tree_sitter::{Parser, Tree};
+
+pub struct BackendData {
+    root_uri: Url,
+    trees: HashMap<Url, Tree>,
+    parser: Parser,
+}
+
+impl BackendData {
+    pub fn set_root_uri(&mut self, root_uri: Url) {
+        self.root_uri = root_uri;
+    }
+}
+
+impl Default for BackendData {
+    fn default() -> Self {
+        let mut parser = Parser::new();
+        parser.set_language(tree_sitter_ccsc::language()).unwrap();
+        Self {
+            root_uri: Url::parse("file:///").unwrap(),
+            trees: Default::default(),
+            parser,
+        }
+    }
+}
+
+pub struct Backend {
+    client: Client,
+    data: Arc<Mutex<BackendData>>,
+}
+
+impl Backend {
+    pub fn new(client: Client) -> Self {
+        let mut parser = Parser::new();
+        parser.set_language(tree_sitter_ccsc::language()).unwrap();
+
+        Self {
+            client,
+            data: Arc::new(Mutex::new(Default::default())),
+        }
+    }
+
+    pub fn get_client(&self) -> &Client {
+        &self.client
+    }
+
+    pub fn get_data(&self) -> Arc<Mutex<BackendData>> {
+        self.data.clone()
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,22 @@
-use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
-
-use tree_sitter::{Language, Parser, Tree, TreeCursor};
-
-use tower_lsp::jsonrpc::{Result, Error, ErrorCode};
+use tower_lsp::jsonrpc::{Error, ErrorCode, Result};
 use tower_lsp::lsp_types::*;
-use tower_lsp::{Client, LanguageServer, LspService, Server};
+use tower_lsp::{LanguageServer, LspService, Server};
 
-use log::{info, error};
-
-struct Data {
-    root_uri: Url,
-    trees: HashMap<Url, Tree>,
-    parser: Parser,
-}
-
-impl Default for Data {
-    fn default() -> Self {
-        let mut parser = Parser::new();
-        parser.set_language(tree_sitter_ccsc::language()).unwrap();
-        Self {
-            root_uri: Url::parse("file:///").unwrap(),
-            trees: Default::default(),
-            parser,
-        }
-    }
-}
-
-struct Backend {
-    client: Client,
-    data: Arc<Mutex<Data>>,
-}
-
-impl Backend {
-    fn new(client: Client) -> Self {
-        let mut parser = Parser::new();
-        parser.set_language(tree_sitter_ccsc::language()).unwrap();
-
-        Self { 
-            client,
-            data: Arc::new(Mutex::new(Default::default())),
-        }
-    }
-}
+mod lsp_ccs_c;
 
 #[tower_lsp::async_trait]
-impl LanguageServer for Backend {
+impl LanguageServer for lsp_ccs_c::Backend {
     async fn initialize(&self, init: InitializeParams) -> Result<InitializeResult> {
         let root_uri = init.root_uri.ok_or(Error::new(ErrorCode::InvalidRequest))?;
 
-        info!("Initializing server: root_uri = {}", root_uri);
+        self.get_client().log_message(
+            MessageType::Info,
+            format!("Initializing server... root_uri = {}", root_uri).as_str(),
+        ).await;
 
-        let mut data = self.data.lock().unwrap();
-        data.root_uri = root_uri;
+        let data_lock = self.get_data();
+        let mut data = data_lock.lock().unwrap();
+        data.set_root_uri(root_uri);
 
         Ok(InitializeResult {
             capabilities: ServerCapabilities {
@@ -69,7 +33,7 @@ impl LanguageServer for Backend {
     }
 
     async fn initialized(&self, _: InitializedParams) {
-        self.client
+        self.get_client()
             .log_message(
                 MessageType::Info,
                 "Server initialized. LSP yet to be fully implemented.",
@@ -84,11 +48,10 @@ impl LanguageServer for Backend {
 
 #[tokio::main]
 async fn main() {
-    log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
     let stdin = tokio::io::stdin();
     let stdout = tokio::io::stdout();
 
-    let (service, messages) = LspService::new(|client| Backend::new(client));
+    let (service, messages) = LspService::new(|client| lsp_ccs_c::Backend::new(client));
     Server::new(stdin, stdout)
         .interleave(messages)
         .serve(service)
